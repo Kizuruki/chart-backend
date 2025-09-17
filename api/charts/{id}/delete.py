842dab8abe719d01f9106475209f327493b394dd
YumYummity <103061664+YumYummity@users.noreply.github.com>
2025-09-17 16:15:44 -0400

    fix: delete route ot use actual session helper

--- api/charts/{id}/delete.py
+++ api/charts/{id}/delete.py
@@ -25,33 +25,14 @@ def setup():
 
         chart_id = id.removeprefix("UnCh-")
 
-        auth = request.headers.get("authorization")
-        # this is a PUBLIC route, don't check for private auth, only user auth
-        if not auth:
+        user = await session.user()
+        if user["banned"]:
             raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED, detail="Not logged in."
+                status_code=status.HTTP_403_FORBIDDEN, detail="User banned."
             )
-        session_data = app.decode_key(auth)
-        if session_data.type != "external":
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN, detail="Invalid token type."
-            )
-        query, args = accounts.generate_get_account_from_session_query(
-            session_data.user_id, auth, "external"
-        )
-        async with app.db.acquire() as conn:
-            result = await conn.fetchrow(query, *args)
-            if not result:
-                raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED, detail="Not logged in."
-                )
-            if result["banned"]:
-                raise HTTPException(
-                    status_code=status.HTTP_403_FORBIDDEN, detail="User banned."
-                )
 
         query, args = charts.generate_delete_chart_query(
-            chart_id, session_data.user_id, confirm_change=True
+            chart_id, session.sonolus_id, confirm_change=True
         )
         async with app.db.acquire() as conn:
             exists = await conn.fetchrow(query, *args)
@@ -59,7 +40,7 @@ def setup():
             async with app.s3_session_getter() as s3:
                 bucket = await s3.Bucket(app.s3_bucket)
                 tasks = []
-                prefix = f"{session_data.user_id}/{chart_id}/"
+                prefix = f"{session.sonolus_id}/{chart_id}/"
                 objects = [obj async for obj in bucket.objects.filter(Prefix=prefix)]
                 if objects:
                     tasks = [obj.delete() for obj in objects]
