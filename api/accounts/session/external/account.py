5f2a0779787eeb406f64e2b9aeae00207d01806b
YumYummity <103061664+YumYummity@users.noreply.github.com>
2025-09-18 00:23:41 -0400

    feat: let external auth grab some basic info

---
+++ api/accounts/session/external/account.py
@@ -0,0 +1,27 @@
+import datetime
+from core import ChartFastAPI
+
+from fastapi import APIRouter, Request, HTTPException, status
+from fastapi.responses import JSONResponse
+
+from database import external
+
+from helpers.session import get_session, Session
+
+router = APIRouter()
+
+
+def setup():
+    @router.get("/")
+    async def main(
+        request: Request,
+        session: Session = get_session(
+            enforce_auth=True, enforce_type="external", allow_banned_users=False
+        ),
+    ):
+        return_keys = ["sonolus_id", "sonolus_handle", "created_at"]
+        return_val = {}
+        for key, value in (await session.user()).items():
+            if key in return_keys:
+                return_val[key] = value
+        return return_val
