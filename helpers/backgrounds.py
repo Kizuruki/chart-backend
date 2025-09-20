f5e60cbe30e73be45f4797e0061144b6527fe664
YumYummity <103061664+YumYummity@users.noreply.github.com>
2025-09-19 22:55:25 -0400

    fix: more logic fixes

--- helpers/backgrounds.py
+++ helpers/backgrounds.py
@@ -8,7 +8,7 @@ from PIL import Image
 
 def generate_backgrounds_resize_jacket(jacket_bytes: bytes):
     jacket_pil_image = Image.open(io.BytesIO(jacket_bytes))
-    jacket_pil_image = jacket_pil_image.resize((640, 640))
+    jacket_pil_image = jacket_pil_image.resize((1000, 1000)).convert("RGBA")
     v1 = pjsk_bg.render_v1(jacket_pil_image)
     v3 = pjsk_bg.render_v3(jacket_pil_image)
     v1_buffer = io.BytesIO()
@@ -20,7 +20,7 @@ def generate_backgrounds_resize_jacket(jacket_bytes: bytes):
     v3_bytes = v3_buffer.getvalue()
     v3_buffer.close()
     jacket_buffer = io.BytesIO()
-    jacket_pil_image.save(jacket_buffer)
+    jacket_pil_image.save(jacket_buffer, format="PNG")
     jacket_bytes = jacket_buffer.getvalue()
     jacket_buffer.close()
     return v1_bytes, v3_bytes, jacket_bytes
